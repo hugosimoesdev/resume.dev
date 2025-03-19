80303a398ae34bca86132aa20f4212b1ec1364dd
Hugo Simões <hugosimoescontato@gmail.com>
2025-03-19 12:36:45 -0300

    feat(api): add resume controller

--- apps/api/src/controllers/create-resume.controller.ts
+++ apps/api/src/controllers/create-resume.controller.ts
@@ -1,22 +1,40 @@
-import { HttpCode, UseGuards } from "@nestjs/common";
+import { Body, UseGuards } from "@nestjs/common";
 import { Controller, Post } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
+import { CurrentUser } from "src/auth/current-user-decorator";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { UserPayload } from "src/auth/jwt.strategy";
+import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
+import { ZodValidationPipe } from "./pipes/zod-validation-pipe";
 
-// const createResumeBodySchema = z.object({
-//   title: z.string(),
-//   description: z.string(),
-// });
+const createResumeBodySchema = z.object({
+  title: z.string(),
+  description: z.string(),
+});
 
-// type CreateResumeBodySchema = z.infer<typeof createResumeBodySchema>;
+type CreateResumeBodySchema = z.infer<typeof createResumeBodySchema>;
+
+const bodyValidationPipe = new ZodValidationPipe(createResumeBodySchema);
 
 @Controller("resumes")
 @UseGuards(JwtAuthGuard)
 export class CreateResumeController {
-  // constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Post()
-  async handle() {
-    return "ok";
+  async handle(
+    @Body(bodyValidationPipe) body: CreateResumeBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, description } = body;
+    const userId = user.sub;
+
+    await this.prisma.resume.create({
+      data: {
+        title,
+        description,
+        userId,
+      },
+    });
   }
 }
